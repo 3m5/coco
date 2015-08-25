9d00fec4292831f6af60eef3857ff11c0c1a3106
wiedenfeld <andreas.wiedenfeld@3m5.de>
2015-08-25 14:40:20 +0200

    increased version

--- src/js/de/_3m5/Coco.Init.js
+++ src/js/de/_3m5/Coco.Init.js
@@ -67,7 +67,7 @@ Coco.SDK = dejavu.Class.declare({
     ////////////////////////////////////////////////////////////
 
     $statics: {
-        version: "0.0.977",
+        version: "0.0.978",
         initialized: false,
         html: false,
         i18n: false
