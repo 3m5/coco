661622c77f926f74a115313723f508bdad8522c3
wiedenfeld <andreas.wiedenfeld@3m5.de>
2016-11-23 10:20:10 +0100

    setup ## 0.1.87

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Coco changelog
 
+## 0.1.87 (unpublished)
+
 ## 0.1.86
 
 - fixed Coco.RouterService history calculation, now it also works for just one step
--- src/js/de/_3m5/Coco.Init.js
+++ src/js/de/_3m5/Coco.Init.js
@@ -156,7 +156,7 @@ Coco.SDK = dejavu.Class.declare({
 	////////////////////////////////////////////////////////////
 
 	$statics: {
-		version: "0.1.86",
+		version: "0.1.87",
 		initialized: false
 	},
 
